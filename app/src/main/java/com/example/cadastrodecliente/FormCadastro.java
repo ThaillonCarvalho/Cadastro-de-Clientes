2215505cb5523167683d669dcc1d1884fa44719c
Thaillon Carvalho <112696502+ThaillonCarvalho@users.noreply.github.com>
2022-09-05 15:04:49 -0300

    Primeiro Commit

---
+++ app/src/main/java/com/example/cadastrodecliente/FormCadastro.java
@@ -0,0 +1,21 @@
+package com.example.cadastrodecliente;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class FormCadastro extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_form_cadastro);
+
+        getSupportActionBar().hide();
+
+    }
+
+
+}
