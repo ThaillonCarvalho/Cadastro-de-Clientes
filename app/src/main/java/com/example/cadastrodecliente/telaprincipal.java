757da6c27d515296a8dc3648881a0210520987e2
Thaillon Carvalho <112696502+ThaillonCarvalho@users.noreply.github.com>
2022-09-05 22:59:19 -0300

    Botao adcionado

--- app/src/main/java/com/example/cadastrodecliente/telaprincipal.java
+++ app/src/main/java/com/example/cadastrodecliente/telaprincipal.java
@@ -10,5 +10,6 @@ public class telaprincipal extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_telaprincipal);
+        getSupportActionBar().hide();
     }
 }
