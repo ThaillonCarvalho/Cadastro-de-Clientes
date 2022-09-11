ccaf1c58e68d6f9c1104ae7acfe7f52b33889456
Thaillon Carvalho <112696502+ThaillonCarvalho@users.noreply.github.com>
2022-09-10 22:44:39 -0300

    tela de cadastro final 3

--- app/src/main/java/com/example/cadastrodecliente/Conexao.java
+++ app/src/main/java/com/example/cadastrodecliente/Conexao.java
@@ -1,2 +1,10 @@
-package com.example.cadastrodecliente;public class Conexao {
+package com.example.cadastrodecliente;
+
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class Conexao {
+
+    public class  conexao extends SQLiteOpenHelper {
+        public  conexao (context context) { super(context,"banco",);}
+    }
 }
